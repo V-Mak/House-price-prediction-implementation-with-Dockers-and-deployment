ea93f6efdb7f4302f13e6ec3ed9d0ffb31eb81fe
Vivek Makwana <111484051+V-Mak@users.noreply.github.com>
2024-01-23 20:25:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,5 +31,5 @@ def predict():
     return render_template('home.html',prediction_text="The House Price Prediction is {}".format(output))
 
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0")
 
